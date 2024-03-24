b84e0300c2a2790ec2c845d8c99fb22fe719fa65
Honduriel <honduriel@googlemail.com>
2024-03-24 08:22:10 +0100

    Added ModConfig.cs

---
+++ SVM_BirthdayNotifications/ModConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SVM_BirthdayNotifications
+{
+    internal class ModConfig
+    {
+        public bool NotifyUnknownNPCs = false;
+    }
+}
